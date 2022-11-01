9a7b7c7af0c4115ec657fa5a7f7b17d3c806bb6e
Alexiy Rybin <0xtapnisu@gmail.com>
2022-11-01 22:33:22 +0700

    Removed screen blinking

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use clap::Parser;
 pub mod utils;
+use std::io::{stdout, Write};
+
+use crossterm::{
+    cursor::{DisableBlinking, EnableBlinking, MoveTo, RestorePosition, SavePosition},
+    execute, ExecutableCommand, Result,
+};
 use std::{fs, thread, time};
 
 #[derive(Parser, Debug)]
@@ -57,7 +63,7 @@ fn main() {
                 for frame in frames {
                     println!("{}", utils::render_frame(frame, args.width, args.reverse));
 
-                    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+                    execute!(stdout(), MoveTo(0, 0));
 
                     thread::sleep(time::Duration::from_millis(frametime));
                 }
@@ -69,7 +75,7 @@ fn main() {
                         utils::render_frame(image_path, args.width, args.reverse)
                     );
 
-                    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+                    execute!(stdout(), MoveTo(0, 0));
 
                     thread::sleep(time::Duration::from_millis(frametime));
                 }
