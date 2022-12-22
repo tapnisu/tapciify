780d809cfa301d0f18d1d4d64bd14e6627c40ba9
Alexiy Rybin <0xtapnisu@gmail.com>
2022-12-22 16:51:01 +0700

    first version

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use std::io::stdout;
 use std::{fs, thread, time};
 use utils::{calc_new_height, render_frame};
 
+use crate::utils::print_colored_frame;
+
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Arguments {
@@ -22,6 +24,8 @@ struct Arguments {
     fps: Option<f64>,
     #[clap(short, long, action)]
     reverse: bool,
+    #[clap(short, long, action)]
+    colored: bool,
 }
 
 fn main() {
@@ -32,79 +36,119 @@ fn main() {
         ascii_string = ascii_string.chars().rev().collect::<String>().to_owned();
     }
 
-    if args.dir {
-        let mut image_paths: Vec<String> = Vec::new();
+    if args.colored {
+        if args.dir {
+            let mut image_paths: Vec<String> = Vec::new();
 
-        let images_paths = fs::read_dir(args.input).unwrap();
-        for image_path in images_paths {
-            match image_path.unwrap().path().to_str() {
-                Some(x) => image_paths.push(x.to_string()),
-                None => println!(),
+            let images_paths = fs::read_dir(args.input).unwrap();
+            for image_path in images_paths {
+                match image_path.unwrap().path().to_str() {
+                    Some(x) => image_paths.push(x.to_string()),
+                    None => println!(),
+                }
             }
-        }
 
-        let frametime: u64;
+            let frametime: u64;
 
-        if let Some(fps) = args.fps {
-            frametime = (1f64 / fps * 1000f64) as u64;
+            if let Some(fps) = args.fps {
+                frametime = (1f64 / fps * 1000f64) as u64;
+            } else {
+                panic!("Frametime is None");
+            }
+
+            let mut image;
+            let mut height;
+
+            for image_path in image_paths {
+                image = image::open(image_path).unwrap().to_rgb8();
+                height = calc_new_height(args.width, image.width(), image.height());
+                print_colored_frame(image.clone(), args.width, height, &ascii_string);
+
+                execute!(stdout(), MoveUp(height as u16 + 1)).expect("");
+
+                thread::sleep(time::Duration::from_millis(frametime));
+            }
         } else {
-            panic!("Frametime is None");
-        }
+            let image = image::open(args.input).unwrap().to_rgb8();
+            let height = calc_new_height(args.width, image.width(), image.height());
 
-        match args.prerender {
-            true => {
-                let mut image;
-                let mut height: Option<u32> = None;
-
-                let mut frames: Vec<String> = Vec::new();
-
-                for image_path in image_paths {
-                    image = image::open(image_path.clone()).unwrap().to_rgb8();
-                    frames.push(render_frame(
-                        image.clone(),
-                        args.width,
-                        calc_new_height(args.width, image.width(), image.height()),
-                        &ascii_string,
-                    ));
-
-                    height = Some(calc_new_height(args.width, image.width(), image.height()));
-
-                    println!("Rendered {}", image_path);
-                }
-
-                for frame in frames {
-                    println!("{}", frame);
-                    if let Some(h) = height {
-                        execute!(stdout(), MoveUp(h as u16 + 1)).expect("");
-                    }
-
-                    thread::sleep(time::Duration::from_millis(frametime));
-                }
-            }
-            false => {
-                let mut image;
-                let mut height;
-
-                for image_path in image_paths {
-                    image = image::open(image_path).unwrap().to_rgb8();
-                    height = calc_new_height(args.width, image.width(), image.height());
-                    println!(
-                        "{}",
-                        render_frame(image.clone(), args.width, height, &ascii_string)
-                    );
-                    execute!(stdout(), MoveUp(height as u16 + 1)).expect("");
-
-                    thread::sleep(time::Duration::from_millis(frametime));
-                }
-            }
+            print_colored_frame(image.clone(), args.width, height, &ascii_string);
         }
     } else {
-        let image = image::open(args.input).unwrap().to_rgb8();
-        let height = calc_new_height(args.width, image.width(), image.height());
+        if args.dir {
+            let mut image_paths: Vec<String> = Vec::new();
 
-        println!(
-            "{}",
-            render_frame(image.clone(), args.width, height, &ascii_string)
-        )
+            let images_paths = fs::read_dir(args.input).unwrap();
+            for image_path in images_paths {
+                match image_path.unwrap().path().to_str() {
+                    Some(x) => image_paths.push(x.to_string()),
+                    None => println!(),
+                }
+            }
+
+            let frametime: u64;
+
+            if let Some(fps) = args.fps {
+                frametime = (1f64 / fps * 1000f64) as u64;
+            } else {
+                panic!("Frametime is None");
+            }
+
+            match args.prerender {
+                true => {
+                    let mut image;
+                    let mut height: Option<u32> = None;
+
+                    let mut frames: Vec<String> = Vec::new();
+
+                    for image_path in image_paths {
+                        image = image::open(image_path.clone()).unwrap().to_rgb8();
+                        frames.push(render_frame(
+                            image.clone(),
+                            args.width,
+                            calc_new_height(args.width, image.width(), image.height()),
+                            &ascii_string,
+                        ));
+
+                        height = Some(calc_new_height(args.width, image.width(), image.height()));
+
+                        println!("Rendered {}", image_path);
+                    }
+
+                    for frame in frames {
+                        println!("{}", frame);
+                        if let Some(h) = height {
+                            execute!(stdout(), MoveUp(h as u16 + 1)).expect("");
+                        }
+
+                        thread::sleep(time::Duration::from_millis(frametime));
+                    }
+                }
+                false => {
+                    let mut image;
+                    let mut height;
+
+                    for image_path in image_paths {
+                        image = image::open(image_path).unwrap().to_rgb8();
+                        height = calc_new_height(args.width, image.width(), image.height());
+                        println!(
+                            "{}",
+                            render_frame(image.clone(), args.width, height, &ascii_string)
+                        );
+                        execute!(stdout(), MoveUp(height as u16 + 1)).expect("");
+
+                        thread::sleep(time::Duration::from_millis(frametime));
+                    }
+                }
+            }
+        } else {
+            let image = image::open(args.input).unwrap().to_rgb8();
+            let height = calc_new_height(args.width, image.width(), image.height());
+
+            println!(
+                "{}",
+                render_frame(image.clone(), args.width, height, &ascii_string)
+            )
+        }
     }
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,6 +1,6 @@
-use std::cmp;
-
+use colored::Colorize;
 use image::RgbImage;
+use std::cmp;
 
 pub fn get_brightness(r: u8, g: u8, b: u8) -> f32 {
     let max = cmp::max(cmp::max(r, g), b);
@@ -20,12 +20,7 @@ pub fn calc_new_height(new_width: u32, width: u32, height: u32) -> u32 {
 }
 
 pub fn render_frame(image: RgbImage, width: u32, height: u32, ascii_string: &str) -> String {
-    let img = image::imageops::resize(
-        &image,
-        width,
-        height,
-        image::imageops::FilterType::Triangle,
-    );
+    let img = image::imageops::resize(&image, width, height, image::imageops::FilterType::Triangle);
 
     let rgb: Vec<u8> = img.into_raw();
 
@@ -48,4 +43,34 @@ pub fn render_frame(image: RgbImage, width: u32, height: u32, ascii_string: &str
     }
 
     frame
+}
+
+pub fn print_colored_frame(image: RgbImage, width: u32, height: u32, ascii_string: &str) {
+    let img = image::imageops::resize(&image, width, height, image::imageops::FilterType::Triangle);
+
+    let rgb: Vec<u8> = img.into_raw();
+
+    let mut x = 0;
+
+    for i in (0..(rgb.len() - 1)).step_by(3) {
+        print!(
+            "{}",
+            ascii_symbol(
+                get_brightness(rgb[i as usize], rgb[i as usize + 1], rgb[i as usize + 2]),
+                ascii_string,
+            )
+            .to_string()
+            .truecolor(rgb[i as usize], rgb[i as usize + 1], rgb[i as usize + 2])
+        );
+
+        x += 1;
+
+        if x == width {
+            print!("\n");
+
+            x = 0;
+        }
+    }
+
+    print!("\n");
 }
