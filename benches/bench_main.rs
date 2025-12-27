ce8043c695d832077a2718a203ed2eab707fe204
Aleksei Rybin <aleksei@tapni.su>
2025-12-28 01:12:02 +0700

    chore: add more benchmarks

---
+++ benches/bench_main.rs
@@ -0,0 +1,54 @@
+use criterion::{Criterion, criterion_group, criterion_main};
+use image::imageops::FilterType;
+use tapciify::{
+    prelude::*,
+    renderers::{
+        ascii::DEFAULT_ASCII_STRING, background_string::BackgroundStringArtConverter,
+        braille::BrailleArtConverter,
+    },
+    utils::resize::DEFAULT_FONT_RATIO,
+};
+
+fn bench_main(c: &mut Criterion) {
+    let img = image::open("./assets/examples/ferris.webp")
+        .unwrap()
+        .resize_custom_ratio(None, None, DEFAULT_FONT_RATIO, FilterType::Triangle);
+
+    c.bench_function("ascii", |b| {
+        let options = AsciiArtConverterOptions::default();
+        b.iter(|| img.ascii_art(&options).unwrap());
+    });
+
+    c.bench_function("colored", |b| {
+        let options = AsciiArtConverterOptions {
+            colored: true,
+            ..Default::default()
+        };
+        b.iter(|| img.ascii_art(&options).unwrap());
+    });
+
+    c.bench_function("background string", |b| {
+        b.iter(|| {
+            img.background_string_art(DEFAULT_ASCII_STRING, false)
+                .unwrap()
+        });
+    });
+
+    c.bench_function("colored background string", |b| {
+        b.iter(|| {
+            img.background_string_art(DEFAULT_ASCII_STRING, true)
+                .unwrap()
+        });
+    });
+
+    c.bench_function("braille", |b| {
+        b.iter(|| img.braille_art(false).unwrap());
+    });
+
+    c.bench_function("colored braille", |b| {
+        b.iter(|| img.braille_art(true).unwrap());
+    });
+}
+
+criterion_group!(benches, bench_main);
+criterion_main!(benches);
